d355c66c227038dae17b46bb3787adf40902df8f
AlaikyaSudhakarYemul <alaikyasudhakar.yemul@wipro.com>
2026-02-24 22:56:30 +0530

    Email dependency added
    Email dependency added and working on email notification

--- backend/DEMP/src/main/java/com/wipro/demp/service/RegistrationServiceImpl.java
+++ backend/DEMP/src/main/java/com/wipro/demp/service/RegistrationServiceImpl.java
@@ -1,9 +1,11 @@
 package com.wipro.demp.service;
 
 
+import com.wipro.demp.entity.Address;
 import com.wipro.demp.entity.Event;
 import com.wipro.demp.entity.RegistrationStatus;
 import com.wipro.demp.entity.Registrations;
+import com.wipro.demp.repository.AddressRepository;
 import com.wipro.demp.repository.EventRepository;
 import com.wipro.demp.repository.RegistrationRepository;
 import com.wipro.demp.entity.Users;
@@ -11,6 +13,9 @@ import com.wipro.demp.repository.UserRepository;
 
 import jakarta.persistence.OptimisticLockException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +31,12 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final RegistrationRepository registrationsRepository;
     private final UserRepository userRepository;
     private final EventRepository eventRepository;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+	private JavaMailSender mailSender;
 
     public RegistrationServiceImpl(RegistrationRepository registrationsRepository, UserRepository userRepository,
             EventRepository eventRepository) {
@@ -67,6 +78,38 @@ public class RegistrationServiceImpl implements RegistrationService {
             registration.getStatus() == null ? RegistrationStatus.REGISTERED : registration.getStatus());
 
         registration.setDeleted(false);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+		message.setTo(registration.getUser().getEmail());
+		message.setSubject("Event Registration Confirmation");
+
+		String msg = "Dear " + registration.getUser().getUserName() + ",\n\n" +
+				"We’re happy to confirm your registration for the event *" + registration.getEvent().getEventName()
+				+ "!* 🎉\n\n" +
+				"Here are your registration details:\n" +
+				"\n" +
+				"*Event Name:* " + registration.getEvent().getEventName() + "\n" +
+				"*Date:* " + registration.getEvent().getDate() + "\n" +
+				"*Event Type:* " + registration.getEvent().getEventType() + "\n";
+                
+
+		if (registration.getEvent().getAddress().getAddressId() <= 0) {
+			Address address = addressService.getAddress(registration.getEvent().getAddress().getAddressId());
+			msg += "*Address:* " + address.getAddress() + ", " + address.getState() + ", " + address.getCountry()
+					+ " - " + address.getPincode() + "\n\n";
+		}
+
+        msg += "\nWe look forward to seeing you there! If you have any questions or need assistance, feel free to contact our support team.\n\n"
+				+
+				"Thank you for choosing *EVENTRA*.\n\n" +
+				"Best regards,\n" +
+				"The EVENTRA Team";
+				
+
+		message.setText(msg);
+		mailSender.send(message);
+
         return registrationsRepository.save(registration);
     }
 
