16394cd896f1490e0e76c62f247fb4860c95f4ef
PravallikaMedisetty <pravallikamedisetty.medisetty@wipro.com>
2026-02-25 14:51:41 +0530

    testing setup done
    testing setup done

---
+++ backend/DEMP/src/test/java/utils/Base.java
@@ -0,0 +1,35 @@
+
+package utils;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+    static public WebDriver driver;
+
+    public void launchBrowser() {
+        Properties prop = ReadProperty.readProperty();
+        if (prop.getProperty("Browser").equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (prop.getProperty("Browser").equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else if (prop.getProperty("Browser").equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        } else {
+            System.out.println("Enter either chrome or firefox or edge");
+        }
+        driver.manage().window().maximize();
+        driver.get(prop.getProperty("URL"));
+    }
+
+    public static void sleep(int msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
