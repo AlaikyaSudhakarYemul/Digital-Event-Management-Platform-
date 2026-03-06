e1457665f489f3ebc026682e34a96e71d0f6380d
PravallikaMedisetty <pravallikamedisetty.medisetty@wipro.com>
2026-03-06 14:32:14 +0530

    Updated home page

--- backend/DEMP/src/test/java/pages/HomePage.java
+++ backend/DEMP/src/test/java/pages/HomePage.java
@@ -1,5 +1,16 @@
 package pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
-
+	private WebDriver driver;
+	private WebDriverWait wait;
+	
+	public HomePage(WebDriver driver) {
+		super();
+		this.driver = driver;
+		wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+	}
 }
