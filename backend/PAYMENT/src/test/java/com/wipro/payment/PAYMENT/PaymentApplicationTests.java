639ad285d86bc4b6a20d48181f9d44e339463785
RajashekarEdulapuram <edulapuram.rajashekar@wipro.com>
2026-04-09 17:18:16 +0530

    Added payment module service
    Added payment module service

---
+++ backend/PAYMENT/src/test/java/com/wipro/payment/PAYMENT/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.payment.PAYMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
